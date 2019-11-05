bae9e7f6ba7da8e013fdd006677f3b956d721926
Propietario <Propietario@pc-1559579848>
2019-11-05 23:10:24 +0100

    Versión inicial

---
+++ src/ud3/ed/UD3ED.java
@@ -0,0 +1,28 @@
+
+package ud3.ed;
+
+/**
+ * @author GJZS
+ */
+public class UD3ED {
+    public static void main (String [] args) {
+        String Estudiante = "Gabriela Zavala";
+        int valor1 = 5;
+        int valor2 = 20;
+        
+      System.out.println("Estudiante "+ args[0] + " : ");
+      System.out.println(veredicto(args[1], args[2]));
+    }
+    static String veredicto (String valor1, String valor2) {
+        if (valor1.equals(valor2)) {
+            return "Bien. Pero una hora de estudio para cada módulo puede ser insuficiente";
+        }
+        if (Integer.valueOf(valor1) < Integer.valueOf(valor2)) {
+            return "Poco tiempo de estudio. Debes dedicar más tiempo.";
+        }
+        return "Ideal. Trabajas los contenidos en casa.";
+        
+        }
+  
+     }   
+   
